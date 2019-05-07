b93a8d4e34fab2aae97b52726daf174773d4d200
Tim Allen <tnelsallen3@gmail.com>
2019-05-07 13:50:50 -0600

    Change 'Food' to 'Foods' in db

--- migrations/20190507194314-create-food.js
+++ migrations/20190507194314-create-food.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Food', {
+    return queryInterface.createTable('Foods', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Food');
+    return queryInterface.dropTable('Foods');
   }
 };
