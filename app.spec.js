ef4c06086026f39f58ede3378ffc9729ac8e6254
Tim Allen <tnelsallen3@gmail.com>
2019-05-07 14:10:20 -0600

    Add test for foods index endpoint and route folder and file
    Co-authored-by: Teresa M Knowles <teresa07moreno@gmail.com>

--- app.spec.js
+++ app.spec.js
@@ -1,3 +1,27 @@
 var shell = require('shelljs');
 var request = require("supertest");
 var app = require('./app');
+
+describe('api', () => {
+  beforeAll(() => {
+    shell.exec('npx sequelize db:create')
+  });
+  beforeEach(() => {
+    shell.exec('npx sequelize db:migrate')
+    shell.exec('npx sequelize db:seed:all')
+  });
+  afterEach(() => {
+    shell.exec('npx sequelize db:migrate:undo:all')
+  });
+
+  describe('Foods index path', () => {
+    test('should return a 200 status', () => {
+      return request(app).get('/api/v1/foods').then(response => {
+        expect(response.status).toBe(200)
+      })
+    });
+    test('should return an array of foods', () => {
+
+    });
+  })
+});
