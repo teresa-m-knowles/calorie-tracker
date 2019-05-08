e3daa11b5b7141f7e0f369d9ad34838c29a259a2
Teresa Knowles <teresa07moreno@gmail.com>
2019-05-08 14:58:48 -0600

    Add Meal table and Meal model
    Co-authored-by: Tim Allen <tnelsallen3@gmail.com>

---
+++ models/meal.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Meal = sequelize.define('Meal', {
+    name: DataTypes.STRING
+  }, {});
+  Meal.associate = function(models) {
+    Meal.belongsToMany(models.Food, {through: 'MealFood'});
+
+  };
+  return Meal;
+};
